71961eaa7dd288d62bad034ef79d908f707daf3f
Wang Hao <675072584@qq.com>
2026-07-16 10:10:54 +0800

    [Add][Day08][02] add harib05b reorganize data and function about reading data from mouse

--- bootpack.c
+++ bootpack.c
@@ -4,14 +4,22 @@
 extern struct FIFO8    keyfifo;
 extern struct FIFO8    mousefifo;
 
-void enable_mouse(void);
+struct MOUSE_DEC
+{
+    unsigned char buf[3], phase;
+    int x, y, btn;
+};
+
+void enable_mouse(struct MOUSE_DEC *mdec);
 void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO*) 0x0ff0;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my;
+    struct MOUSE_DEC mdec;
 
     init_gdtidt();
     init_pic();
@@ -38,9 +46,7 @@ void HariMain(void)
 
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-    enable_mouse();
-    int mouse_phase = 0;
-    unsigned char mouse_dbuf[3];
+    enable_mouse(&mdec);
 
 	for (;;) {
 		io_cli();
@@ -57,21 +63,9 @@ void HariMain(void)
             }
             else if(fifo8_status(&mousefifo) != 0) {
                 int data = fifo8_get(&mousefifo);
-                if(mouse_phase == 0) {
-                    if(data == 0xfa) mouse_phase = 1;
-                }
-                else if(mouse_phase == 1) {
-                    mouse_dbuf[0] = data;
-                    mouse_phase = 2;
-                }
-                else if(mouse_phase == 2) {
-                    mouse_dbuf[1] = data;
-                    mouse_phase = 3;
-                }
-                else if(mouse_phase == 3) {
-                    mouse_dbuf[2] = data;
-                    mouse_phase = 1;
-                    sprintf(s, "%02X %02X %2X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2] );
+                io_sti();
+                if(mouse_decode(&mdec, data) != 0) {
+                    sprintf(s, "%02X %02X %2X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
                     boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32+8*8-1, 31);
                     putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
                 }
@@ -101,13 +95,43 @@ void wait_KBC_sendready(void)
     return;
 }
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
     wait_KBC_sendready();
     io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
     wait_KBC_sendready();
     io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    mdec->phase = 0;
     return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+    if(mdec->phase == 0) {
+        if(dat == 0xfa) mdec->phase = 1;
+        return 0;
+    }
+
+    if(mdec->phase == 1) {
+        mdec->buf[0] = dat;
+        mdec->phase = 2;
+        return 0;
+    }
+
+    if(mdec->phase == 2) {
+        mdec->buf[1] = dat;
+        mdec->phase = 3;
+        return 0;
+    }
+
+    if(mdec->phase == 3) {
+        mdec->buf[2] = dat;
+        mdec->phase = 1;
+
+        return 1;
+    }
+
+    return -1;    
 }
 
 
